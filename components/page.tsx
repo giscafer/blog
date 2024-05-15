30deb441e1ce7865d343f55862274a8a860ba5cb
giscafer <giscafer@outlook.com>
2024-05-15 23:48:11 +0800

    fix: YouTube link

--- components/page.tsx
+++ components/page.tsx
@@ -11,7 +11,7 @@ type PageProps = {
 const footerLinks = [
   { name: 'Home', url: '/' },
   { name: 'GitHub', url: 'https://github.com/giscafer', target: '_blank' },
-  { name: 'YouTube', url: 'https://www.youtube.com/@BeAFreelancer', target: '_blank' },
+  { name: 'YouTube', url: 'https://www.youtube.com/@LeekHuber', target: '_blank' },
 
   { name: 'Blog', url: '/blog' },
   { name: 'Twitter', url: 'https://twitter.com/nicky_lao', target: '_blank' },
